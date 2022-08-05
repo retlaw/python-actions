1a3ed7a9d4394a683b4004e21f13f978a87db6bf
Walter <scharnick@gmail.com>
2022-08-05 17:02:47 +0200

    updated fttb-checker.py

--- .github/workflows/fttb-checker.py
+++ .github/workflows/fttb-checker.py
@@ -1,6 +1,9 @@
+from urllib import response
+import requests
 
 def main():
-  print("Where is the fttbs?")
+  response = requests.get(url='http://13.244.137.122/?ip=196.213.34.97&count=1')
+  print(response.json())
 
 if __name__ == "__main__":
   main()

e11134bc8bf102316cd6e783700f99c5ea20a408
Walter <scharnick@gmail.com>
2022-08-06 09:15:36 +0200

    Added Vredendal

--- fttb-checker.py
+++ fttb-checker.py
@@ -4,14 +4,14 @@ import requests
 
 fttb_data = {
   "Grabouw": "41.23.75.105",
-  "Long Street": "196.213.32.201"
+  "Long Street": "196.213.32.201",
+  "Vredendal": "41.23.143.57"
 }
 fttb_pings = {}
 def main():
   for branch in fttb_data:
     response = requests.get(url=f'http://13.244.137.122/?ip={fttb_data[branch]}&count=1')
     fttb_pings[branch] = response.json()
-  # print(response.json())
   print(fttb_pings)
 
 if __name__ == "__main__":

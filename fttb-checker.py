fcd59abb700cda5d17b8b96575cfa7ea2db2ecef
Walter <scharnick@gmail.com>
2022-08-06 12:10:41 +0200

    Added more Branches

--- fttb-checker.py
+++ fttb-checker.py
@@ -5,7 +5,14 @@ import requests
 fttb_data = {
   "Grabouw": "41.23.75.105",
   "Long Street": "196.213.32.201",
-  "Vredendal": "41.23.143.57"
+  "Vredendal": "41.23.143.57",
+  "Ottery": "41.23.103.65",
+  "De Doorns": "41.23.102.17",
+  "Epping": "196.213.35.201",
+  "Vredenburg": "41.23.102.241",
+  "Moorreesburg": "41.23.88.129",
+  "Paarl": "196.214.43.137",
+  "Houtbay": "41.23.92.249"
 }
 fttb_pings = {}
 def main():

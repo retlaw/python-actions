ab187142347c92179a8d24dfa15df8f7136197b1
Walter <scharnick@gmail.com>
2022-08-06 08:59:30 +0200

    Moved main script and added more Branches to be checked.

--- .github/workflows/fttb-checker.py
+++
@@ -1,9 +0,0 @@
-from urllib import response
-import requests
-
-def main():
-  response = requests.get(url='http://13.244.137.122/?ip=196.213.34.97&count=1')
-  print(response.json())
-
-if __name__ == "__main__":
-  main()
---
+++ fttb-checker.py
@@ -0,0 +1,18 @@
+import re
+from urllib import response
+import requests
+
+fttb_data = {
+  "Grabouw": "41.23.75.105",
+  "Long Street": "196.213.32.201"
+}
+fttb_pings = {}
+def main():
+  for branch in fttb_data:
+    response = requests.get(url=f'http://13.244.137.122/?ip={fttb_data[branch]}&count=1')
+    fttb_pings[branch] = response.json()
+  # print(response.json())
+  print(fttb_pings)
+
+if __name__ == "__main__":
+  main()

9dd4e59c7e91c7e13bf6527937c550a4672a71d5
Walter <scharnick@gmail.com>
2022-08-07 06:25:14 +0200

    added env to python script yaml

--- fttb-checker.py
+++ fttb-checker.py
@@ -1,32 +1,38 @@
 import requests
 from datetime import datetime
-import json, os
+import json
+import os
 
 fttb_data = {
-  "Grabouw": "41.23.75.105",
-  "Long Street": "196.213.32.201",
-  "Vredendal": "41.23.143.57",
-  "Ottery": "41.23.103.65",
-  "De Doorns": "41.23.102.17",
-  "Epping": "196.213.35.201",
-  "Vredenburg": "41.23.102.241",
-  "Moorreesburg": "41.23.88.129",
-  "Paarl": "196.214.43.137",
-  "Houtbay": "41.23.92.249"
+    "Grabouw": "41.23.75.105",
+    "Long Street": "196.213.32.201",
+    "Vredendal": "41.23.143.57",
+    "Ottery": "41.23.103.65",
+    "De Doorns": "41.23.102.17",
+    "Epping": "196.213.35.201",
+    "Vredenburg": "41.23.102.241",
+    "Moorreesburg": "41.23.88.129",
+    "Paarl": "196.214.43.137",
+    "Houtbay": "41.23.92.249"
 }
 fttb_pings = list()
+
+
 def main():
-  for branch in fttb_data:
-    count = 1
-    response = requests.get(url=f'http://13.244.137.122/?ip={fttb_data[branch]}&count={count}')
-    fttb_pings.append({
-      branch: {
-        "result": response.json(),
-        "count": count,
-        "datetime": datetime.now().isoformat()
-        }})
-  print(json.dumps(fttb_pings))
-  print(os.environ.get('SUPER_SECRET'))
+    for branch in fttb_data:
+        count = 1
+        response = requests.get(
+            url=f'http://13.244.137.122/?ip={fttb_data[branch]}&count={count}')
+        fttb_pings.append({
+            branch: {
+                "result": response.json(),
+                "count": count,
+                "datetime": datetime.now().isoformat()
+            }})
+    print(json.dumps(fttb_pings))
+    secret = os.environ.get('SUPER_SECRET')
+    print(f'the secret is {secret}')
+
 
 if __name__ == "__main__":
-  main()
+    main()

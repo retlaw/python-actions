4ddef17e03d75df2c3010f6ebec5b906e0bc7ecd
Walter <scharnick@gmail.com>
2022-10-13 20:00:12 +0200

    Update fttb-checker.py

--- fttb-checker.py
+++ fttb-checker.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import json
 import os
 
+
 fttb_data = {
     "Grabouw": "41.23.75.105",
     "Long Street": "196.213.32.201",
@@ -13,8 +14,31 @@ fttb_data = {
     "Vredenburg": "41.23.102.241",
     "Moorreesburg": "41.23.88.129",
     "Paarl": "196.214.43.137",
-    "Houtbay": "41.23.92.249"
+    "Houtbay": "41.23.92.249",
+	"Cape Town City": "196.213.34.97",
+	"N1 City": "196.213.52.201",
+	"Neelsie": "196.213.52.57",
+	"Bredasdorp": "196.213.52.41",
+	"Caledon": "196.213.53.249",
+	"Century City": "196.214.155.153",
+	"Stellenbosch Commercial Secondary": "196.213.221.97",
+	"Gardens": "196.214.143.241",
+	"Piketberg": "41.23.75.161",
+	"Willowbridge": "196.214.140.97",
+	"Wellington": "41.23.128.233",
+	"Tulbagh": "41.23.128.9",
+	"Strand": "196.214.137.177",
+	"StillBay": "41.23.103.73",
+	"Stellenbosch": "196.213.107.113",
+	"Springbok": "41.23.200.201",
+	"Somerset West": "196.214.137.137",
+	"Robertson": "196.214.137.193",
+	"Promenade": "196.214.154.129",
+	"Malmesbury": "41.23.102.65",
+	"Khayelitsha": "41.23.92.121",
+	"Greenpoint": "41.23.102.97"
 }
+
 fttb_pings = list()
 
 

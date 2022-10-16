272fafe9158b2de7b9cb04858d232efe3acba264
Walter <scharnick@gmail.com>
2022-10-16 18:30:41 +0200

    Added Branches

--- fttb-checker.py
+++ fttb-checker.py
@@ -21,7 +21,9 @@ fttb_data = {
     "Bredasdorp": "196.213.52.41",
     "Caledon": "196.213.53.249",
     "Century City": "196.214.155.153",
+    "Stellenbosch Commercial Primary": "41.164.74.137",
     "Stellenbosch Commercial Secondary": "196.213.221.97",
+    "Worcester Commercial": "41.164.154.201",
     "Gardens": "196.214.143.241",
     "Piketberg": "41.23.75.161",
     "Willowbridge": "196.214.140.97",
@@ -38,7 +40,23 @@ fttb_data = {
     "Khayelitsha": "41.23.92.121",
     "Greenpoint": "41.23.102.97",
     "TableBayMall": "196.213.222.81",
-    "Myhouse": "41.23.101.101"
+    "Parow": "41.164.182.209",
+    "Cape Gate": "196.213.53.169",
+    "Tygervalley": "196.213.209.145",
+    "Worcester": "196.213.216.249",
+    "Somerset Mall": "196.213.55.233",
+    "Kenilworth": "196.213.53.25",
+    "Rondebosch": "196.214.137.185",
+    "Bredasdorp": "196.213.52.42",
+    "Watergate": "196.214.159.113",
+    "Tokai": "196.214.144.81",
+    "Vineyard": "196.214.148.145",
+    "Villiersdorp": "41.23.103.8",
+    "LongBeachMall": "196.214.63.121",
+    "Junxion Mall": "41.23.74.217",
+    "Ceres": "41.23.74.161",
+    "ClanWilliam": "41.23.74.105",
+    "Brackenfell": "196.214.43.33"
 }
 
 fttb_pings = list()

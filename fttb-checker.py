225c56934a9853fce8a2e54b7038044899cc2be8
Walter <scharnick@gmail.com>
2023-02-10 06:41:43 +0200

    Fixed IP address typo for Zevenwagcht

--- fttb-checker.py
+++ fttb-checker.py
@@ -7,7 +7,7 @@ import os
 fttb_data = {
     "Mosselbaai": "196.214.167.73",
     "Vangate": "196.214.166.153",
-    "Zevenwagcht": "96.214.167.153",
+    "Zevenwagcht": "196.214.167.153",
     "Riversdale": "196.214.168.129",
     "Grabouw": "41.23.75.105",
     "Long Street": "196.213.32.201",
